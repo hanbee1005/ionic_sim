56dfc50d016d20040095f98843f31d0a6c760037
hanbee <sonhanbi1002@gmail.com>
2020-06-04 22:11:19 +0900

    Get Sim Info 구현

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+import { Sim } from '@ionic-native/sim/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  simInfo;
 
+  constructor(private platform: Platform,
+              private sim: Sim) {}
+
+  getSimInfo() {
+    this.platform.ready().then(() => {
+      const result = this.checkPermission();
+
+      if (result) {
+        this.sim.getSimInfo().then(
+            (info) => {
+              console.log('Sim info: ', info);
+              this.simInfo = info;
+            },
+            (err) => console.log('Unable to get sim info: ', err)
+        );
+      }
+    });
+  }
+
+  checkPermission() {
+    if (this.platform.is('android')) {
+      // Android Only
+      this.sim.hasReadPermission().then((info) => {
+            console.log('Has permission: ', info);
+            if (info) {
+              return true;
+            } else {
+              this.sim.requestReadPermission().then(
+                  () => {
+                    console.log('Permission granted');
+                    return true;
+                  },
+                  () => {
+                    console.log('Permission denied');
+                    return false;
+                  }
+              );
+            }
+          }
+      ).catch(err => {
+        console.log(err);
+        return false;
+      });
+    } else {
+      return true;
+    }
+  }
 }
